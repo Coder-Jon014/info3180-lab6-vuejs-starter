6cc97870533e1d10a0caf931c88ea7fb0760030c
Coder-Jon014 <jon.coombs14@gmail.com>
2022-04-03 01:57:06 -0500

    Added NewsList

---
+++ src/components/NewsList.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul class="news__list">
+        <li class="news__item">News item 1</li>
+        <li class="news__item">News item 2</li>
+        <li class="news__item">News item 3</li>       
+    </ul>
+</template>
+
+<script>
+export default{
+    data() {
+        return {};
+    },
+
+    create() {
+        fetch('https://newsapi.org/v2/top-headlines?country=us', {
+            headers: {
+                'Authorization': 'Bearer ' + '4cf8802b014549bba25ff992f7cf3976'
+            }
+        })
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+        })
+    }
+};
+</script>
